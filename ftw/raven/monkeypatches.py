f46b7853b25f851d36d50743ffc2d59a2a387bc0
Jonas Baumann <jone@jone.ch>
2016-01-14 08:59:35 +0100

    patch __repr__ onto ZApplicationWrapper
    ZApplicationWrapper has no __repr__ because it does not inherit
    from object.
    For having stack locals in sentry, the raven client tries to make
    repr() for each object, which fails with the ZApplicationWrapper
    because there is no implementation.
    Therefore we add the __repr__ to the ZApplicationWrapper class.

--- ftw/raven/monkeypatches.py
+++ ftw/raven/monkeypatches.py
@@ -1,3 +1,4 @@
+from App.ZApplication import ZApplicationWrapper
 from ftw.raven.reporter import maybe_report_exception
 from plone.app.linkintegrity import monkey
 from plone.app.linkintegrity.monkey import zpublisher_exception_hook_wrapper
@@ -14,6 +15,21 @@ def zpublisher_exception_hook_wrapper_wrapper(published, REQUEST,
                                              t, v, traceback)
 
 
+def ZApplicationWrapper__repr__(self):
+    """ZApplicationWrapper has no __repr__ because it does not inherit
+    from object.
+    For having stack locals in sentry, the raven client tries to make
+    repr() for each object, which fails with the ZApplicationWrapper
+    because there is no implementation.
+    Therefore we add the __repr__ to the ZApplicationWrapper class.
+    """
+    mod = self.__class__.__module__
+    cls = self.__class__.__name__
+    mem = '0x' + hex(id(self))[2:].zfill(8).upper()
+    return '<{0}.{1} instance at {2}>'.format(mod, cls, mem)
+
+
 def install_patches():
     monkey.zpublisher_exception_hook_wrapper = (
         zpublisher_exception_hook_wrapper_wrapper)
+    ZApplicationWrapper.__repr__ = ZApplicationWrapper__repr__
